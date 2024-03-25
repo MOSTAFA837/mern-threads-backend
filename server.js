0b7a2fcb12b916020c236368c645b754f5dcce93
MOSTAFA837 <mostafamanchester837@gmail.com>
2024-03-26 01:58:29 +0200

    initial server, models, routes

---
+++ server.js
@@ -0,0 +1,22 @@
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
+import { connectDB } from "./db/connectDB.js";
+import userRoutes from "./routes/userRoutes.js";
+
+dotenv.config();
+connectDB();
+
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+// routes
+app.use("/api/users", userRoutes);
+
+app.listen(500, () => console.log(`Server is listening at port ${port}`));

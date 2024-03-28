c271a938a472b2349d71e84118a1ab0d6d09f912
MOSTAFA837 <mostafamanchester837@gmail.com>
2024-03-28 02:49:26 +0200

    user follow and unfollow

--- controllers/userController.js
+++ controllers/userController.js
@@ -90,3 +90,45 @@ export const logout = async (req, res) => {
     console.log("Error in signupUser: ", error.message);
   }
 };
+
+export const followUnfollow = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    console.log("name", req.user.name);
+
+    const userToModify = await User.findById(id);
+    const currentUser = await User.findById(req.user._id);
+
+    console.log("userToModify", userToModify);
+
+    if (!userToModify || !currentUser) {
+      return res.status(400).json({ error: "User not found" });
+    }
+
+    if (id === req.user._id.toString()) {
+      return res
+        .status(400)
+        .json({ error: "You cannot follow/unfollow yourself" });
+    }
+
+    const isFollowing = currentUser.following.includes(id);
+
+    if (isFollowing) {
+      // unfollow user
+      await User.findByIdAndUpdate(id, { $pull: { followers: req.user._id } });
+      await User.findByIdAndUpdate(req.user._id, {
+        $pull: { following: id },
+      });
+      res.status(200).json({ message: "User unfollowed successfully" });
+    } else {
+      // follow user
+      await User.findByIdAndUpdate(id, { $push: { followers: req.user._id } });
+      await User.findByIdAndUpdate(req.user._id, { $push: { following: id } });
+      res.status(200).json({ message: "User followed successfully" });
+    }
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+    console.log("Error in followUnFollowUser: ", error.message);
+  }
+};
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,10 +1,18 @@
 import express from "express";
-import { signup, login, logout } from "../controllers/userController.js";
+import {
+  signup,
+  login,
+  logout,
+  followUnfollow,
+} from "../controllers/userController.js";
+import { authMiddleware } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
 router.post("/signup", signup);
 router.post("/login", login);
 router.post("/logout", logout);
+
+router.post("/follow/:id", authMiddleware, followUnfollow);
 
 export default router;

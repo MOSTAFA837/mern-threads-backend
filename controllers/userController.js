d30ba89f40463a5842b320ea493858252acb5cc0
MOSTAFA837 <mostafamanchester837@gmail.com>
2024-03-28 03:05:54 +0200

    get and update user profile

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,33 @@
 import bcrypt from "bcryptjs";
+import mongoose from "mongoose";
 
 import User from "../models/userModel.js";
 import { generateTokenAndSetCookie } from "../utils/generateTokenAndSetCookie.js";
+
+export const getUserProfile = async (req, res) => {
+  const { query } = req.params;
+
+  try {
+    let user;
+
+    if (mongoose.Types.ObjectId.isValid(query)) {
+      user = await User.findById(query)
+        .select("-password")
+        .select("-updatedAt");
+    } else {
+      user = await User.findOne({ username: query })
+        .select("-password")
+        .select("-updatedAt");
+    }
+
+    if (!user) return res.status(404).json({ error: "User not found" });
+
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+    console.log("erroror in getUserProfile: ", err.message);
+  }
+};
 
 export const signup = async (req, res) => {
   try {
@@ -88,6 +114,44 @@ export const logout = async (req, res) => {
   } catch (error) {
     res.status(500).json({ error: error.message });
     console.log("Error in signupUser: ", error.message);
+  }
+};
+
+export const update = async (req, res) => {
+  const { name, email, username, password, bio } = req.body;
+
+  let profilePic;
+
+  const userId = req.user._id;
+
+  try {
+    let user = await User.findById(userId);
+    if (!user) return res.status(400).json({ error: "User not found" });
+
+    if (req.params.id !== userId.toString()) {
+      return res
+        .status(400)
+        .json({ error: "You cannot update other user's profile" });
+    }
+
+    if (password) {
+      const salt = await bcrypt.genSalt(10);
+      const hashedPassword = await bcrypt.hash(password, salt);
+      user.password = hashedPassword;
+    }
+
+    user.name = name || user.name;
+    user.email = email || user.email;
+    user.username = username || user.username;
+    user.profilePic = profilePic || user.profilePic;
+    user.bio = bio || user.bio;
+
+    user = await user.save();
+
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+    console.log("Error in updateUser: ", error.message);
   }
 };
 
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,15 +4,20 @@ import {
   login,
   logout,
   followUnfollow,
+  update,
+  getUserProfile,
 } from "../controllers/userController.js";
 import { authMiddleware } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
+
+router.get("/profile/:query", getUserProfile);
 
 router.post("/signup", signup);
 router.post("/login", login);
 router.post("/logout", logout);
 
 router.post("/follow/:id", authMiddleware, followUnfollow);
+router.post("/update/:id", authMiddleware, update);
 
 export default router;
